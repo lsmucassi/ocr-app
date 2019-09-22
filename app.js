3b2266580fea074ae86edd39d19ba17a0e608031
lsmucassi <lindasmucassi@gmail.com>
2019-09-22 20:02:03 +0200

    starting: app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const multer = require("multer");
+const {TesseractWorker} = require("tesseract.js");
+const worker = new TesseractWorker();
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cd(null, "./uploads")
+    },
+    filename: (req, res, cb) => {
+        cb(null, req.file)
+    }
+});
+
